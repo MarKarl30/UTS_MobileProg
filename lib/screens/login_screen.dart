91b6fc5cca4904399056b1e2537216094133aa4c
ohakoo <jtamaraputra@gmail.com>
2024-09-22 15:44:01 +0700

    Changes in Register & Login Screen - Removed the navbar - Added validations to ensure correct values inputted by user

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -16,139 +16,183 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  final _formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
-  bool _obscurePassword = true; // Controls whether password is obscured or not
+  final otpController = TextEditingController();
+  bool _obscurePassword = true; // Controls whether OTP is obscured or not
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-      ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset('assets/images/login_img.png'),
-              const SizedBox(height: 16),
-              const Text(
-                'Login Details',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 24.0),
-              TextField(
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 1.0,
-                      color: Colors.black,
-                    ),
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 1.0,
-                      color: Colors.black,
-                    ),
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                  hintText: 'Enter Phone Number or Email',
-                  hintStyle: const TextStyle(
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              const Text('Request for OTP'),
-              const SizedBox(height: 16),
-              TextField(
-                keyboardType: TextInputType.number,
-                obscureText: _obscurePassword, // Controls password visibility
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _obscurePassword
-                          ? Icons.visibility
-                          : Icons.visibility_off,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _obscurePassword =
-                            !_obscurePassword; // Toggle password visibility
-                      });
-                    },
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 1.0,
-                      color: Colors.black,
-                    ),
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 1.0,
-                      color: Colors.black,
-                    ),
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                  hintText: 'Enter One-Time Password',
-                  hintStyle: const TextStyle(
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 30.0),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  padding: const EdgeInsets.symmetric(vertical: 14.0),
-                ),
-                child: const Text(
-                  'Login',
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset('assets/images/login_img.png'),
+                const SizedBox(height: 16),
+                const Text(
+                  'Login Details',
                   style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 24.0),
-              const Text(
-                'Don\'t have an account yet?',
-                textAlign: TextAlign.center,
-              ),
-              TextButton(
-                onPressed: () {
-                  navigateToRegisterScreen(context);
-                },
-                style: TextButton.styleFrom(
-                  textStyle: const TextStyle(
+                    fontSize: 24,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                child: const Text(
-                  'Sign up',
+                const SizedBox(height: 24.0),
+                TextFormField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 1.0,
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 1.0,
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 1.0,
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 1.0,
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    hintText: 'Enter Phone Number or Email',
+                    hintStyle: const TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email or phone number';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 10.0),
+                const Text('Request for OTP'),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: otpController,
+                  keyboardType: TextInputType.number,
+                  obscureText: _obscurePassword, // Controls password visibility
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscurePassword
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      },
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 1.0,
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 1.0,
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 1.0,
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 1.0,
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    hintText: 'Enter One-Time Password',
+                    hintStyle: const TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the OTP';
+                    } else if (value.length != 6) {
+                      return 'OTP must be exactly 6 digits';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 30.0),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Perform login action
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 14.0),
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 24.0),
+                const Text(
+                  'Don\'t have an account yet?',
                   textAlign: TextAlign.center,
                 ),
-              ),
-            ],
+                TextButton(
+                  onPressed: () {
+                    navigateToRegisterScreen(context);
+                  },
+                  style: TextButton.styleFrom(
+                    textStyle: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  child: const Text(
+                    'Sign up',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -28,20 +28,23 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final _formKey = GlobalKey<FormState>();
   final textController = TextEditingController();
+  final phoneController = TextEditingController();
+  final emailController = TextEditingController();
   final pinController = TextEditingController();
   final confirmPinController = TextEditingController();
   bool _obscurePassword = true;
 
-  // Function to build a TextField to avoid repetition
   Widget buildTextField({
     required TextEditingController controller,
     required String hintText,
+    required String? Function(String?) validator,
     bool obscureText = false,
     TextInputType keyboardType = TextInputType.text,
     TextInputAction textInputAction = TextInputAction.done,
   }) {
-    return TextField(
+    return TextFormField(
       controller: controller,
       obscureText: obscureText,
       keyboardType: keyboardType,
@@ -61,6 +64,20 @@ class _RegisterState extends State<Register> {
           ),
           borderRadius: BorderRadius.circular(5.0),
         ),
+        errorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(
+            width: 1.0,
+            color: Colors.red, // Error color
+          ),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(
+            width: 1.0,
+            color: Colors.red, // Error color
+          ),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
         hintText: hintText,
         hintStyle: const TextStyle(
           color: Colors.grey,
@@ -72,126 +89,169 @@ class _RegisterState extends State<Register> {
                 ),
                 onPressed: () {
                   setState(() {
-                    _obscurePassword =
-                        !_obscurePassword; // Toggle password visibility
+                    _obscurePassword = !_obscurePassword; // Toggle password visibility
                   });
                 },
               )
             : null,
       ),
+      validator: validator,
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register'),
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-      ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset('assets/images/register_img.jpg'),
-              const SizedBox(height: 16),
-              const Text(
-                'Sign Up Now!',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 24.0),
-              buildTextField(
-                controller: textController,
-                hintText: 'Enter Phone Number',
-                keyboardType: TextInputType.number,
-                textInputAction: TextInputAction.next,
-              ),
-              const SizedBox(height: 7.0),
-              const Text('Verify Phone Number'),
-              const SizedBox(height: 10),
-              buildTextField(
-                controller: textController,
-                hintText: 'Enter Email (optional)',
-                keyboardType: TextInputType.emailAddress,
-                textInputAction: TextInputAction.next,
-              ),
-              const Text('Verify Email'),
-              const SizedBox(height: 16),
-              buildTextField(
-                controller: pinController,
-                keyboardType: TextInputType.number,
-                hintText: 'Create PIN',
-                obscureText: true,
-                textInputAction: TextInputAction.next,
-              ),
-              const SizedBox(height: 16),
-              buildTextField(
-                controller: confirmPinController,
-                keyboardType: TextInputType.number,
-                hintText: 'Confirm PIN',
-                obscureText: true,
-              ),
-              TextButton(
-                onPressed: () {
-                  navigateToHomeScreen(context);
-                  navigateToNavigation(context);
-                },
-                style: TextButton.styleFrom(
-                  textStyle: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                child: const Text(
-                  'Continue Without Account?',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  padding: const EdgeInsets.symmetric(vertical: 14.0),
-                ),
-                child: const Text(
-                  'Register',
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset('assets/images/register_img.jpg'),
+                const SizedBox(height: 16),
+                const Text(
+                  'Sign Up Now!',
                   style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 24.0),
-              const Text(
-                'Already have an account?',
-                textAlign: TextAlign.center,
-              ),
-              TextButton(
-                onPressed: () {
-                  navigateToLoginScreen(context);
-                },
-                style: TextButton.styleFrom(
-                  textStyle: const TextStyle(
+                    fontSize: 24,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                child: const Text(
-                  'Login',
+                const SizedBox(height: 24.0),
+                buildTextField(
+                  controller: textController,
+                  hintText: 'Enter Full Name (Sesuai KTP)',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your full name';
+                    }
+                    return null;
+                  },
+                  keyboardType: TextInputType.text,
+                  textInputAction: TextInputAction.next,
+                ),
+                const SizedBox(height: 16.0),
+                buildTextField(
+                  controller: phoneController,
+                  hintText: 'Enter Phone Number',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your phone number';
+                    }
+                    return null;
+                  },
+                  keyboardType: TextInputType.number,
+                  textInputAction: TextInputAction.next,
+                ),
+                const SizedBox(height: 7.0),
+                const Text('Verify Phone Number'),
+                const SizedBox(height: 10),
+                buildTextField(
+                  controller: emailController,
+                  hintText: 'Enter Email (optional)',
+                  validator: (value) {
+                    if (value != null && value.isNotEmpty && !RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                      return 'Please enter a valid email';
+                    }
+                    return null;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                ),
+                const Text('Verify Email'),
+                const SizedBox(height: 16),
+                buildTextField(
+                  controller: pinController,
+                  keyboardType: TextInputType.number,
+                  hintText: 'Create PIN',
+                  obscureText: true,
+                  textInputAction: TextInputAction.next,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a PIN';
+                    } else if (value.length < 4) {
+                      return 'PIN must be at least 4 digits';
+                    } else if (!RegExp(r'^[0-9]+$').hasMatch(value)) { // PIN harus berupa angka
+                      return 'PIN must be a number';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                buildTextField(
+                  controller: confirmPinController,
+                  keyboardType: TextInputType.number,
+                  hintText: 'Confirm PIN',
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please confirm your PIN';
+                    } else if (value != pinController.text) {
+                      return 'PINs do not match';
+                    }
+                    return null;
+                  },
+                ),
+                TextButton(
+                  onPressed: () {
+                    navigateToHomeScreen(context);
+                    navigateToNavigation(context);
+                  },
+                  style: TextButton.styleFrom(
+                    textStyle: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  child: const Text(
+                    'Continue Without Account?',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Perform registration action
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 14.0),
+                  ),
+                  child: const Text(
+                    'Register',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 24.0),
+                const Text(
+                  'Already have an account?',
                   textAlign: TextAlign.center,
                 ),
-              ),
-            ],
+                TextButton(
+                  onPressed: () {
+                    navigateToLoginScreen(context);
+                  },
+                  style: TextButton.styleFrom(
+                    textStyle: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  child: const Text(
+                    'Login',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
